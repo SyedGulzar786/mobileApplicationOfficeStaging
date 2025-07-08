f452b8d6323c3a36eb01d41c70c3f92b17b34254
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-07-08 04:01:57 -0700

    models / Attendance.js  updated
    new logic as regarding signedin and signedout functionality begins

--- backend/models/Attendance.js
+++ backend/models/Attendance.js
@@ -1,3 +1,19 @@
+// const mongoose = require('mongoose');
+
+// const attendanceSchema = new mongoose.Schema({
+//   userId: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'User',
+//   },
+//   date: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
+
+// module.exports = mongoose.model('Attendance', attendanceSchema);
+
+
 const mongoose = require('mongoose');
 
 const attendanceSchema = new mongoose.Schema({
@@ -7,8 +23,18 @@ const attendanceSchema = new mongoose.Schema({
   },
   date: {
     type: Date,
-    default: Date.now,
+    required: true,
   },
+  signInAt: {
+    type: Date,
+    default: null,
+  },
+  signOutAt: {
+    type: Date,
+    default: null,
+  }
 });
+
+attendanceSchema.index({ userId: 1, date: 1 }, { unique: true });
 
 module.exports = mongoose.model('Attendance', attendanceSchema);
