6b80d06235d6271aaa7a2db4554d9163269253c9
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-07-30 09:46:50 -0700

    try to solve web cors issue

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,11 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:8081', // your frontend origin
+  credentials: true,              // allow cookies & headers
+}));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static('public'));
