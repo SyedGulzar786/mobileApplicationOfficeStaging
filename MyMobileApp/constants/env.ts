2671c199078dca88ccbf63c3b03aaa6631d01a1f
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-08-21 15:52:17 -0700

    Revert "changed ipconfig address to home"
    This reverts commit 72584a75f9b16bbe199c1c16b3e966d2e4c32d47.

--- MyMobileApp/constants/env.ts
+++ MyMobileApp/constants/env.ts
@@ -1,11 +1,7 @@
-// MyMobileApp/constants/env.ts # home
-export const API_BASE_URL = 'http://192.168.1.105:5000'; // your office IP
-export const ALLOWED_IP = '192.168.100.174'; // restrict login to this IP
-
-// // MyMobileApp/constants/env.ts # office
+// // MyMobileApp/constants/env.ts
 // export const API_BASE_URL = 'http://192.168.100.174:5000'; // your office IP
 // export const ALLOWED_IP = '192.168.100.174'; // restrict login to this IP
 
-// // MyMobileApp/constants/env.ts # production
-// export const API_BASE_URL = 'https://attendance.isoftik.com'; // your office IP
-// export const ALLOWED_IP = '192.168.100.174'; // restrict login to this IP
+// MyMobileApp/constants/env.ts
+export const API_BASE_URL = 'https://attendance.isoftik.com'; // your office IP
+export const ALLOWED_IP = '192.168.100.174'; // restrict login to this IP
