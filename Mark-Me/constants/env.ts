0717a47214fd3721bb0b7787a87780bfdd0acd45
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-09-10 18:02:30 -0700

    Update env.ts

--- Mark-Me/constants/env.ts
+++ Mark-Me/constants/env.ts
@@ -1,7 +1,7 @@
 // // MyMobileApp/constants/env.ts
-export const API_BASE_URL = 'http://192.168.1.102:5000'; // your office IP
+// export const API_BASE_URL = 'http://192.168.1.102:5000'; // your office IP
 // export const ALLOWED_IP = '192.168.100.174'; // restrict login to this IP
 
 // MyMobileApp/constants/env.ts
-// export const API_BASE_URL = 'https://attendance.isoftik.com'; // your office IP
+export const API_BASE_URL = 'https://attendance.isoftik.com'; // your office IP
 export const ALLOWED_IP = '192.168.100.174'; // restrict login to this IP
